0c7a7ca703e46694fa466588e4d021a1f99562c4
kenaiiscute <matildafahle@hotmail.com>
2026-04-16 10:05:33 +0200

    adding screens
    test to see is I have access

---
+++ screens/login_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final nameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green.shade100,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Skogsjakten 🌿", style: TextStyle(fontSize: 30)),
+
+              const SizedBox(height: 30),
+
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: "Vad heter du?",
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => HomeScreen(name: nameController.text),
+                    ),
+                  );
+                },
+                child: const Text("Starta"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
