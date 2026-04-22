282f955674e875c2e94c03d1286521f7e732cf37
kenaiiscute <matildafahle@hotmail.com>
2026-04-22 11:39:25 +0200

    Create user, fixat utseende med rätt grön bakgrund, lagt till ett fält för namn, gula rutor. Den testar att lösenordet inte är för lätt, att bekräfta lösenord stämmer och att email ser ut som en email. Kopplas till vår databas för användare så man faktiskt registerar en ny användare.

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
+import 'home_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class CreateAccount extends StatefulWidget {
   const CreateAccount({super.key});
@@ -11,13 +14,38 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   final nameController = TextEditingController();
   final passwordController = TextEditingController ();
+  final emailController = TextEditingController();
   final confirmPasswordController = TextEditingController();
+
   final _formKey = GlobalKey<FormState>();
+
+  Future<bool> registerUser(String name, String email, String password) async {
+    final response = await http.post(
+      Uri.parse('https://group-6-15.pvt.dsv.su.se/auth/register'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'name': name,
+        'email': email,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      print('Konto skapat!: ${response.body}');
+      return true;
+    } else if (response.statusCode == 409) {
+      print('Användaren finns redan');
+      return false;
+    } else {
+      print('Fel: ${response.body}');
+      return false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF84C06C),
+      backgroundColor: Color(0xFFBEDBB2),
       body: Align(
         alignment: Alignment.topCenter,
         child: Form(
@@ -42,9 +70,17 @@ class _CreateAccountState extends State<CreateAccount> {
                 vertical: 15.0,
               ),
               child: TextFormField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: "Vad heter du?",
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Mejladress",
+                  filled: true,
+                  fillColor: Color(0xFFF8ED76),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
                 validator: (value){ // olika validations
                   if(value == null || value.isEmpty) return "Ogiltig mejl";
@@ -56,17 +92,31 @@ class _CreateAccountState extends State<CreateAccount> {
               ),
             ),
 
-
+            // Name
             Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 20.0,
                 vertical: 15.0,
               ),
-              child: TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  labelText: "Lösenord",
+              child: TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: "Användarnamn",
+                  filled: true,
+                  fillColor: Color(0xFFF8ED76),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Vänligen skriv in ditt namn";
+                    }
+                    return null;
+                  }
               ),
             ),
 
@@ -75,28 +125,112 @@ class _CreateAccountState extends State<CreateAccount> {
                 horizontal: 20.0,
                 vertical: 15.0,
               ),
-              child: TextField(
-                controller: confirmPasswordController,
-                decoration: const InputDecoration(
-                  labelText: "Bekräfta lösenord",
+              child: TextFormField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: "Lösenord",
+                  filled: true,
+                  fillColor: Color(0xFFF8ED76),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
                 ),
+                  validator: (value){
+                    if(value == null || value.isEmpty) return "Ogiltigt lösenord";
+                    if(value.length <= 10) return "Lösenordet måste vara minst 10 tecken";
+                    if(!value.contains(RegExp(r'[A-Z]'))) return "Lösenordet måste innehålla en stor bokstav";
+                    if(!value.contains(RegExp(r'[0-9]'))) return "Lösenordet måste innehålla en siffra";
+                    return null;
+                    }
+                ),
+              ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 15.0,
+              ),
+              child: TextFormField(
+                controller: confirmPasswordController,
+                decoration: InputDecoration(
+                  labelText: "Bekräfta lösenord",
+                  filled: true,
+                  fillColor: Color(0xFFF8ED76),
+                  contentPadding: EdgeInsets.symmetric(vertical: 16),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Bekräfta lösenord";
+                  }
+
+                  if (value != passwordController.text) {
+                    return "Lösenorden matchar inte";
+                  }
+
+                  return null;
+                }
               ),
             ),
 
             const SizedBox(height: 40),
 
             ElevatedButton(
-              onPressed: () {
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFFB1067E),
+                foregroundColor: Colors.black,
+              ),
+              onPressed: () async {
                 if (_formKey.currentState!.validate()) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => LoginScreen(),
-                    ),
+
+                  bool success = await registerUser(
+                    nameController.text,
+                    emailController.text,
+                    passwordController.text,
                   );
+
+                  if (success) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Konto skapat!")),
+                    );
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => HomeScreen(name: nameController.text),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text("Användaren finns redan eller fel uppstod"),
+                      ),
+                    );
+                  }
                 }
               },
               child: const Text("Skapa konto"),
+            ),
+
+            const SizedBox(height: 20),
+
+            TextButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const LoginScreen()),
+                );
+              },
+              child: const Text("Tillbaka till login"),
             ),
           ],
           ),
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,7 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF84C06C),
+      backgroundColor: Color(0xFFBEDBB2),
       body: Stack(
         children: [
 
