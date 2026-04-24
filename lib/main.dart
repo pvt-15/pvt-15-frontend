8690ee8cb736a1086bc03029c7b3f6a07ee39ec4
yousra159 <yousrabencherifalundberg@gmail.com>
2026-04-24 12:53:50 +0200

    fredag arbete. artprofil + fixad hemskärm

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'screens/home_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-//test2
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,28 +13,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Skogsjakten',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
-
-        textTheme: TextTheme(
+        scaffoldBackgroundColor: const Color(0xFFBEDBB2),
+        textTheme: const TextTheme(
           headlineLarge: TextStyle(
             fontFamily: 'YoungSerif',
             fontSize: 30,
+            color: Color(0xFF4C290C),
           ),
           headlineMedium: TextStyle(
             fontFamily: 'YoungSerif',
             fontSize: 24,
+            color: Color(0xFF4C290C),
+          ),
+          titleLarge: TextStyle(
+            fontFamily: 'YoungSerif',
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF4C290C),
           ),
           titleMedium: TextStyle(
             fontFamily: 'WinkySans',
-            fontSize: 16,
+            fontSize: 18,
+            color: Color(0xFF4C290C),
           ),
           bodyMedium: TextStyle(
             fontFamily: 'WinkySans',
             fontSize: 16,
+            color: Color(0xFF4C290C),
           ),
         ),
-
       ),
       home: const LoginScreen(),
     );
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -101,9 +101,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
                         validator: (value){
                           if(value == null || value.isEmpty) return "Ogiltigt lösenord";
-                          if(value.length <= 10) return "Lösenordet måste vara minst 10 tecken";
-                          if(!value.contains(RegExp(r'[A-Z]'))) return "Lösenordet måste innehålla en stor bokstav";
-                          if(!value.contains(RegExp(r'[0-9]'))) return "Lösenordet måste innehålla en siffra";
+                          //if(value.length <= 10) return "Lösenordet måste vara minst 10 tecken";
+                          //if(!value.contains(RegExp(r'[A-Z]'))) return "Lösenordet måste innehålla en stor bokstav";
+                          //if(!value.contains(RegExp(r'[0-9]'))) return "Lösenordet måste innehålla en siffra";
                           return null;
                         }
                         ),
