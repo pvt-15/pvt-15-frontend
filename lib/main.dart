12238e7bac57899debadc214ea7066b178f32bd1
yousra159 <yousrabencherifalundberg@gmail.com>
2026-05-05 11:41:06 +0200

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:Skogsjakten/screens/home.dart';
 import 'package:Skogsjakten/services/session_storage.dart';
 import 'package:Skogsjakten/services/session.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
-//test2
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +24,30 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xFFBEDBB2),
         textTheme: const TextTheme(
           headlineLarge: TextStyle(
-            fontFamily: 'YoungSerif',
-            fontSize: 30,
-            color: Color(0xFF4C290C),
-          ),
+              fontFamily: 'YoungSerif', fontSize: 30, color: Color(0xFF4C290C)),
           headlineMedium: TextStyle(
-            fontFamily: 'YoungSerif',
-            fontSize: 24,
-            color: Color(0xFF4C290C),
-          ),
-          titleLarge: TextStyle(
-            fontFamily: 'YoungSerif',
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: Color(0xFF4C290C),
+              fontFamily: 'YoungSerif', fontSize: 24, color: Color(0xFF4C290C)),
+          titleLarge: TextStyle(fontFamily: 'YoungSerif',
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFF4C290C)),
+          bodyMedium: TextStyle(
+              fontFamily: 'WinkySans', fontSize: 16, color: Color(0xFF4C290C)),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF84C06C),
+            foregroundColor: const Color(0xFF4C290C),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15)),
           ),
         ),
         inputDecorationTheme: const InputDecorationTheme(
           filled: true,
           fillColor: Color(0xFFF8ED76),
           contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20))),
         ),
       ),
 
@@ -50,12 +55,6 @@ class MyApp extends StatelessWidget {
       home: FutureBuilder<Session?>(
         future: SessionStorage().get(),
         builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            );
-          }
-
           final session = snapshot.data;
 
           if (session != null && session.token.isNotEmpty) {
@@ -64,18 +63,10 @@ class MyApp extends StatelessWidget {
 
           return const LoginScreen();
         },
-          titleMedium: TextStyle(
-            fontFamily: 'WinkySans',
-            fontSize: 18,
-            color: Color(0xFF4C290C),
-          ),
-          bodyMedium: TextStyle(
-            fontFamily: 'WinkySans',
-            fontSize: 16,
-            color: Color(0xFF4C290C),
-          ),
-        ),
       ),
     );
   }
 }
+    //home : const LoginScreen(),
+
+
--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import '../../Authorization/user_model.dart';
+import 'reset_password.dart';
+import '../home.dart';
+import 'create_account.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:Skogsjakten/repositories/auth_repository.dart';
-import 'package:Skogsjakten/services/token_storage.dart';
-import 'package:Skogsjakten/services/user_local_storage.dart';
-import 'package:Skogsjakten/Authorization/user_model.dart';
 import '../skogsjakten_exception.dart';
-import '../home.dart';
-import 'reset_password.dart';
-import 'create_account.dart';
+import 'package:Skogsjakten/services/session_storage.dart';
+import 'package:Skogsjakten/services/session.dart';
+
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -21,32 +21,43 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final nameController = TextEditingController();
   final passwordController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-  final String serverClientId = '171324929378-o6f6ehfj8vtte1fasnhdd2jnjf376uto.apps.googleusercontent.com';
+  final _formKey = GlobalKey<
+      FormState>(); //lägg till denna för att valideringen ska fungera vid användarnamn formfield.
+  String serverClientId = '171324929378-o6f6ehfj8vtte1fasnhdd2jnjf376uto.apps.googleusercontent.com';
 
-  final authRepository = AuthRepository(
-    tokenStorage: TokenStorage(),
-    userLocalStorage: UserLocalStorage(),
-  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: const Color(0xFFBEDBB2),
+      //testa om detta löser hoppandet med "nytt konto" osv
+      backgroundColor: Color(0xFFBEDBB2),
       body: Stack(
         children: [
+
           Positioned(
             top: 190,
             left: 0,
             right: 0,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("Skogsjakten", style: Theme.of(context).textTheme.headlineLarge),
-                Image.asset('assets/maskot_skogstroll.png', width: 90, height: 90),
+                Text(
+                    "Skogsjakten",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headlineLarge
+                ),
+                Image.asset(
+                  'assets/maskot_skogstroll.png',
+                  width: 90,
+                  height: 90,
+                ),
               ],
             ),
           ),
+
           Positioned(
             top: 360,
             left: 0,
@@ -54,10 +65,47 @@ class _LoginScreenState extends State<LoginScreen> {
             child: Form(
               key: _formKey,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  _buildTextField(nameController, "Email"),
-                  _buildTextField(passwordController, "Lösenord", obscure: true),
-                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 10.0,
+                    ),
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        labelText: "Email",
+                      ),
+
+                      validator: (value) { // olika validations
+                        if (value == null || value.isEmpty)
+                          return "Ogiltig mejl";
+                        return null;
+                      },
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20.0,
+                      vertical: 15.0,
+                    ),
+                    child: TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: "Lösenord",
+                        ),
+
+                        validator: (value) {
+                          if (value == null || value.isEmpty)
+                            return "Ogiltigt lösenord";
+                          return null;
+                        }
+                    ),
+                  ),
+
                   ElevatedButton(
                     onPressed: _handleLogin,
                     child: const Text("Logga in"),
@@ -66,47 +114,105 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
           ),
+
           Positioned(
-            bottom: 190,
-            left: 0,
-            right: 0,
-            child: Center(
-              child: ElevatedButton(
-                onPressed: _handleGoogleLogin,
-                child: const Text("Logga in med Google"),
-              ),
-            ),
+              bottom: 190,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final result = await signIn();
+
+                        if (result == null)
+                          return; // Användaren avbröt inloggningen
+
+                        bool success = await loginWithGoogle(
+                            result['idToken']!);
+
+                        if (success) {
+                          if (!mounted) return;
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (_) =>
+                                  HomeScreen(
+                                      name: result['name'] ?? "Användare"),
+                            ),
+                          );
+                        } else {
+                          if (!mounted) return;
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text(
+                                "Inloggning misslyckad",
+                                textAlign: TextAlign.center)),
+                          );
+                        }
+                      } catch (e) {
+                        debugPrint(e.toString());
+                        if (!mounted) return;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text(
+                              "Något gick fel", textAlign: TextAlign.center)),
+                        );
+                      }
+                    },
+                    child: const Text("Logga in med Google"),
+
+                  )
+                ],
+              )
+
           ),
+
           Positioned(
             bottom: 70,
             left: 0,
             right: 0,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const CreateAccount())),
-                  child: const Text("Skapa konto"),
+
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => CreateAccount(),
+                        ),
+                      );
+                    },
+                    child: const Text("Skapa konto"),
+                  ),
                 ),
-                TextButton(
-                  onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (_) => const ResetPassword())),
-                  child: const Text("Glömt lösenord?"),
+
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 10
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ResetPassword(),
+                        ),
+                      );
+                    },
+                    child: const Text("Glömt lösenord?"),
+                  ),
                 ),
               ],
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _buildTextField(TextEditingController controller, String label, {bool obscure = false}) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      child: TextFormField(
-        controller: controller,
-        obscureText: obscure,
-        decoration: InputDecoration(labelText: label),
-        validator: (value) => (value == null || value.isEmpty) ? "Fältet får inte vara tomt" : null,
       ),
     );
   }
@@ -122,47 +228,90 @@ class _LoginScreenState extends State<LoginScreen> {
         }),
       );
 
+
       if (response.statusCode == 200 && mounted) {
         final data = jsonDecode(response.body);
-        await authRepository.saveLoginData(
-          token: data['token'],
-          user: UserModel(
-            userId: data['userId'].toString(),
-            username: data['name'],
-            email: data['email'],
+
+        await SessionStorage().save(
+          Session(
+            token: data['token'],
+            user: UserModel(
+              userId: data['userId'].toString(),
+              username: data['name'],
+              email: data['email'],
+            ),
           ),
         );
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen(name: nameController.text)));
+
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => HomeScreen(name: data['name']),
+          ),
+        );
       } else if (mounted) {
-        _showError("Fel email eller lösenord");
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+              "Fel email eller lösenord",
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
       }
     }
   }
 
-  Future<void> _handleGoogleLogin() async {
-    try {
-      final GoogleSignIn googleSignIn = GoogleSignIn(serverClientId: serverClientId);
-      final account = await googleSignIn.signIn();
-      if (account == null) return;
 
-      final auth = await account.authentication;
-      final response = await http.post(
-        Uri.parse('https://group-6-15.pvt.dsv.su.se/auth/google'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'token': auth.idToken}),
+  Future<Map<String, String?>?> signIn() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn.instance;
+
+      await googleSignIn.initialize(
+        serverClientId: serverClientId,
       );
 
-      if (response.statusCode == 200 && mounted) {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen(name: account.displayName ?? "Användare")));
-      } else if (mounted) {
-        _showError("Google-inloggning misslyckad");
+
+      final GoogleSignInAccount? account = await googleSignIn.authenticate();
+
+      if (account == null) return null;
+
+      final GoogleSignInAuthentication auth = await account.authentication;
+
+      final String? idToken = auth.idToken;
+
+      if (idToken == null) {
+        throw SkogsjaktenException("Ingen token mottagen från Google");
       }
+
+      debugPrint('DEBUG: Mottagen google id token: $idToken');
+
+      return {
+        'idToken': idToken,
+        'name': account.displayName,
+      };
     } catch (e) {
-      _showError("Ett fel uppstod: $e");
+      debugPrint("Google Sign-In Error: $e");
+      rethrow;
     }
   }
 
-  void _showError(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message, textAlign: TextAlign.center)));
+  Future<bool> loginWithGoogle(String idToken) async {
+    final response = await http.post(
+      Uri.parse('https://group-6-15.pvt.dsv.su.se/auth/google'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'token': idToken}),
+    );
+
+    debugPrint("Backend statuskod: ${response.statusCode}");
+    debugPrint("Backend svar: ${response.body}");
+
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      print('Fel lösenord eller email: ${response.body}');
+      return false;
+    }
   }
 }
