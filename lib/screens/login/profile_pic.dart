e70e60922f482b6ee78767b0a5f14704665aee98
yousra159 <yousrabencherifalundberg@gmail.com>
2026-05-07 10:45:59 +0200

    auth
    # Conflicts:
    #	lib/main.dart

--- lib/screens/login/profile_pic.dart
+++ lib/screens/login/profile_pic.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyPage extends StatelessWidget {
-  const MyPage({super.key});
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({super.key});
 
   @override
   Widget build(BuildContext context) {
