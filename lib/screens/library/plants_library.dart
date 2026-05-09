ddae4ad0f3f462205dd578a1e5a397ecec6fdfc8
kenaiiscute <matildafahle@hotmail.com>
2026-05-09 19:08:41 +0200

    Gjort merge, ändrat error och fixat navigationbar så man kommer till profil. Bingo och quiz väljer man svårighetsgrad.

--- lib/screens/library/plants_library.dart
+++ lib/screens/library/plants_library.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../services/session_storage.dart';
-import 'package:flutter/material.dart';
 
 class PlantPicture {
   final int id;
