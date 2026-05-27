178313a8eba756127b69b85544d51e619b44ee1f
kenaiiscute <matildafahle@hotmail.com>
2026-05-27 15:55:57 +0200

    Ändrat quiz så när man väljer ett alternativ blir rutan vit och inte grön direkt.

--- lib/screens/home/home_library.dart
+++ lib/screens/home/home_library.dart
@@ -31,7 +31,9 @@ class _HomeLibraryState extends State<HomeLibrary> {
       'image': 'assets/Icons/medalj.png',
       'page': const MedalsLibrary(),
     },
-    {'name': 'Dagens utmaning', 'image': 'assets/Icons/medalj.png', 'page': const DailyLibrary()},
+    {'name': 'Dagens utmaning',
+      'image': 'assets/Icons/medalj.png',
+      'page': const DailyLibrary()},
   ];
 
   @override
