9dcab6c0a56ef969f38a2d4da69edea91d5abaa3
kenaiiscute <matildafahle@hotmail.com>
2026-05-29 14:14:41 +0200

    Lagt in lite översättningar

--- lib/services/translation_service.dart
+++ lib/services/translation_service.dart
@@ -11,6 +11,14 @@ class TranslationService {
   static final Map<String, String> _manualTranslations = {
     'Daisy': 'Prästkrage',
     'Dog' : 'Hund',
+    'Dairy cattle' : 'Ko',
+    'Japanese flowering cherry' : 'Körsbärsträd',
+    'Ladybird beetle' : 'Nyckelpiga',
+    'Gulls' : 'Fiskmås',
+    'Common lilac' : 'Syren',
+    'Garden Pansy' : 'Penséer',
+    'Sweet William' : 'Nejlikor',
+
   };
 
   // debugging - skriv ut manuella översättningar
